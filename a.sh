0376bf76f215e7fab45347e1ab5b1ed6ec365afb
nobita54 <a.k639583027@gmail.com>
2025-12-06 01:51:53 +0530

    Create a.sh

---
+++ a.sh
@@ -0,0 +1,7 @@
+echo "# thm" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/nobita54/thm.git
+git push -u origin main
